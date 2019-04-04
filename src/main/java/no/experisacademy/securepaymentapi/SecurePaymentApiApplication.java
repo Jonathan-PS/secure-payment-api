f1d5cd4562dc97e37cb04854d47a40054aa26ab4
JonathanPS <jonathanpstang@gmail.com>
2019-04-04 11:15:41 +0200

    first commit

---
+++ src/main/java/no/experisacademy/securepaymentapi/SecurePaymentApiApplication.java
@@ -0,0 +1,13 @@
+package no.experisacademy.securepaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePaymentApiApplication.class, args);
+	}
+
+}
