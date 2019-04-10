1ed589db61f0a82d9b927e24b3efe89df6818af5
Helene <helene.harmens@gmail.com>
2019-04-10 16:04:06 +0200

    added Stripe to Gradle, and managed to get a connection to Stripe. I created a couple of simple methods that can create a customer (to Stripe) and print out all customers.

---
+++ src/main/java/no/experisacademy/securepaymentapi/models/Checkout.java
@@ -0,0 +1,11 @@
+package no.experisacademy.securepaymentapi.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+public class Checkout implements Serializable {
+
+    private String STRIPE_PUBLIC_KEY = "pk_test_pt1UnWeg7M8aXk1Qh8Ef5UmM00NyCvXYL4";
+
+}
