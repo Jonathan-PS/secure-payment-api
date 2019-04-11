0c6118e3bd544cca0c3dec7a05aa053b2428dd6d
Philippe Rasay <guerrerophilippe@gmail.com>
2019-04-11 15:52:35 +0200

    Bugfix. Fixed cors on UserOrderController. Renamed java files from Order to UserOrder.

--- src/main/java/no/experisacademy/securepaymentapi/controllers/UserOrderController.java
+++ src/main/java/no/experisacademy/securepaymentapi/controllers/UserOrderController.java
@@ -3,16 +3,14 @@ package no.experisacademy.securepaymentapi.controllers;
 import no.experisacademy.securepaymentapi.models.UserOrder;
 import no.experisacademy.securepaymentapi.repositories.UserOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserOrderController {
 
