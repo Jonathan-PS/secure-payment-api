e69d0abf683a3bc6f52f6d4206204d77fa313d13
Philippe Rasay <guerrerophilippe@gmail.com>
2019-04-26 16:38:38 +0200

    unfinished work

--- src/main/java/no/experisacademy/securepaymentapi/config/SecurityConfig.java
+++ src/main/java/no/experisacademy/securepaymentapi/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package no.experisacademy.securepaymentapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +26,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers( "/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
+        http
+                .cors()
                 .and()
-                .formLogin().permitAll();
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/products/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                ;
+
+                /*.antMatchers( "/**").access("hasRole('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();*/
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
